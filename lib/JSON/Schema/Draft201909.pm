27e9a213b415e5f25eeddc0e3f87d02f2020bfe0
Karen Etheridge <ether@cpan.org>
2020-04-10 10:53:16 -0700

    fix version guard on indirect

--- lib/JSON/Schema/Draft201909.pm
+++ lib/JSON/Schema/Draft201909.pm
@@ -1,6 +1,6 @@
 use strict;
 use warnings;
-no if "$]" >= 5.031008, feature => 'indirect';
+no if "$]" >= 5.031009, feature => 'indirect';
 package JSON::Schema::Draft201909;
 # vim: set ts=8 sts=4 sw=4 tw=115 et :
 # ABSTRACT: Validate data against a schema
