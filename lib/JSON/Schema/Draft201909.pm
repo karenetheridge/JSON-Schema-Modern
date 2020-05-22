93254535983d046c99054667dccd05a9b65ee9f0
Karen Etheridge <ether@cpan.org>
2020-05-22 12:35:42 -0700

    set keyword in state before any errors are generated for that keyword

--- lib/JSON/Schema/Draft201909.pm
+++ lib/JSON/Schema/Draft201909.pm
@@ -121,9 +121,10 @@ sub _eval {
   ) {
     next if not exists $schema->{$keyword};
 
+    $state->{keyword} = $keyword;
     my $method = '_eval_keyword_'.($keyword =~ s/^\$//r);
     abort($state, 'unsupported keyword "%s"', $keyword) if not $self->can($method);
-    $result = 0 if not $self->$method($data, $schema, +{ %$state, keyword => $keyword });
+    $result = 0 if not $self->$method($data, $schema, $state);
 
     return 0 if not $result and $state->{short_circuit};
   }
