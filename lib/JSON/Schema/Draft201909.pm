67fead52fac0e2090c132e8ca68d25d7ab31c8c9
Karen Etheridge <ether@cpan.org>
2020-06-10 14:26:52 -0700

    squash: include the regex checker here

--- lib/JSON/Schema/Draft201909.pm
+++ lib/JSON/Schema/Draft201909.pm
@@ -1466,7 +1466,8 @@ To date, missing components include most of these. More specifically, features t
 
 =head1 SECURITY CONSIDERATIONS
 
-The C<pattern> and C<patternProperties> keywords evaluate regular expressions from the schema.
+The C<pattern> and C<patternProperties> keywords, and the C<regex> format validator,
+evaluate regular expressions from the schema.
 No effort is taken (at this time) to sanitize the regular expressions for embedded code or
 potentially pathological constructs that may pose a security risk, either via denial of service
 or by allowing exposure to the internals of your application. B<DO NOT RUN SCHEMAS FROM UNTRUSTED
