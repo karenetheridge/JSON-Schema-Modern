b675a1c88ed3547a3eca36fbf926a63e3937cbff
Karen Etheridge <ether@cpan.org>
2020-06-21 21:24:47 -0700

    increment $VERSION after 0.008 release

--- lib/JSON/Schema/Draft201909/Document.pm
+++ lib/JSON/Schema/Draft201909/Document.pm
@@ -4,7 +4,7 @@ package JSON::Schema::Draft201909::Document;
 # vim: set ts=8 sts=2 sw=2 tw=100 et :
 # ABSTRACT: One JSON Schema document
 
-our $VERSION = '0.008';
+our $VERSION = '0.009';
 
 no if "$]" >= 5.031009, feature => 'indirect';
 use feature 'current_sub';
