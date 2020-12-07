8ef2673bed9e11636a0a01d4e46178537b2a1142
Karen Etheridge <ether@cpan.org>
2020-12-07 10:26:29 -0800

    increment $VERSION after 0.018 release

--- lib/JSON/Schema/Draft201909/Utilities.pm
+++ lib/JSON/Schema/Draft201909/Utilities.pm
@@ -4,7 +4,7 @@ package JSON::Schema::Draft201909::Utilities;
 # vim: set ts=8 sts=2 sw=2 tw=100 et :
 # ABSTRACT: Internal utilities for JSON::Schema::Draft201909
 
-our $VERSION = '0.018';
+our $VERSION = '0.019';
 
 use 5.016;
 no if "$]" >= 5.031009, feature => 'indirect';
