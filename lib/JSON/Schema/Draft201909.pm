070a5f3c90e7efbe7527bbb40d72846021057b6d
Karen Etheridge <ether@cpan.org>
2020-12-08 10:41:16 -0800

    increment $VERSION after 0.019 release

--- lib/JSON/Schema/Draft201909.pm
+++ lib/JSON/Schema/Draft201909.pm
@@ -5,7 +5,7 @@ package JSON::Schema::Draft201909;
 # ABSTRACT: Validate data against a schema
 # KEYWORDS: JSON Schema data validation structure specification
 
-our $VERSION = '0.019';
+our $VERSION = '0.020';
 
 use 5.016;  # for fc, unicode_strings features
 no if "$]" >= 5.031009, feature => 'indirect';
