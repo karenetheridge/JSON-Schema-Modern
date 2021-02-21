9fae560643f71c504c911b65e4669fe92dfc7760
Karen Etheridge <ether@cpan.org>
2021-02-21 10:37:39 -0800

    increment $VERSION after 0.023 release

--- lib/JSON/Schema/Draft201909/Vocabulary.pm
+++ lib/JSON/Schema/Draft201909/Vocabulary.pm
@@ -4,7 +4,7 @@ package JSON::Schema::Draft201909::Vocabulary;
 # vim: set ts=8 sts=2 sw=2 tw=100 et :
 # ABSTRACT: Base role for JSON Schema vocabulary classes
 
-our $VERSION = '0.023';
+our $VERSION = '0.024';
 
 use 5.016;
 no if "$]" >= 5.031009, feature => 'indirect';
