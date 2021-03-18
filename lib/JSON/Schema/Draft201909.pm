16440849e4d49ac3887c126a18528b6121226b1b
Karen Etheridge <ether@cpan.org>
2021-03-18 12:41:12 -0700

    validate_formats once again defaults to false, as per the spec
    see https://json-schema.org/draft/2019-09/json-schema-validation.html#rfc.section.7.2.2
    
    "Regardless of the boolean value of the vocabulary declaration, an
    implementation that can evaluate "format" as an assertion MUST provide options
    to enable and disable such evaluation. The assertion evaluation behavior when
    the option is not explicitly specified depends on the vocabulary declaration's
    boolean value."
    
    ..and (for now) we only support the default $schema, where the format vocabulary is false.
    
    This partially reverts commit db2a6825fbb1c9dbc01a0d34a6603dc56b3ca62f validate_formats now defaults to true

--- lib/JSON/Schema/Draft201909.pm
+++ lib/JSON/Schema/Draft201909.pm
@@ -55,7 +55,7 @@ has max_traversal_depth => (
 has validate_formats => (
   is => 'ro',
   isa => Bool,
-  default => 1,
+  default => 0, # as specified by https://json-schema.org/draft/2019-09/schema#/$vocabulary
 );
 
 has collect_annotations => (
@@ -580,10 +580,8 @@ other, or badly-written schemas that could be optimized. Defaults to 50.
 
 =head2 validate_formats
 
-When false, the C<format> keyword will be treated as an annotation only (that is, no evaluation
-failure will result if the format of the data string is not as specified). When true, the C<format>
-keyword will be treated as an assertion, where failure is possible.
-Defaults to true.
+When true, the C<format> keyword will be treated as an assertion, not merely an annotation. Defaults
+to false.
 
 =head2 format_validations
 
--- t/zzz-acceptance.t
+++ t/zzz-acceptance.t
@@ -36,7 +36,7 @@ my $accepter = Test::JSON::Schema::Acceptance->new(
   verbose => 1,
 );
 
-my %options = ();
+my %options = (validate_formats => 1);
 my $js = JSON::Schema::Draft201909->new(%options);
 my $js_short_circuit = JSON::Schema::Draft201909->new(%options, short_circuit => 1);
 
