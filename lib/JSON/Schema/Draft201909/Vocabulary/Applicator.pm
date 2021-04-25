d8017f7a9c49878205cda1bc7ca970b3d9c72f2a
Karen Etheridge <ether@cpan.org>
2021-04-24 17:51:05 -0700

    bump version to 0.128

--- lib/JSON/Schema/Draft201909/Vocabulary/Applicator.pm
+++ lib/JSON/Schema/Draft201909/Vocabulary/Applicator.pm
@@ -4,7 +4,7 @@ package JSON::Schema::Draft201909::Vocabulary::Applicator;
 # vim: set ts=8 sts=2 sw=2 tw=100 et :
 # ABSTRACT: Implementation of the JSON Schema Draft 2019-09 Applicator vocabulary
 
-our $VERSION = '0.029';
+our $VERSION = '0.128';
 
 use 5.016;
 no if "$]" >= 5.031009, feature => 'indirect';
