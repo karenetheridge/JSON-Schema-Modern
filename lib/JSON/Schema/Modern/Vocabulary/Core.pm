879d70703a0ee660f3a074301df492b5826ef77a
Karen Etheridge <ether@cpan.org>
2021-11-19 19:12:14 -0800

    reorder some $vocabulary checks

--- lib/JSON/Schema/Modern/Vocabulary/Core.pm
+++ lib/JSON/Schema/Modern/Vocabulary/Core.pm
@@ -247,18 +247,18 @@ sub _traverse_keyword_vocabulary ($self, $schema, $state) {
   return E($state, '$vocabulary can only appear at the schema resource root')
     if length($state->{schema_path});
 
-  return E($state, '$vocabulary can only appear at the document root')
+  my $valid = 1;
+  $valid = E($state, '$vocabulary can only appear at the document root')
     if length($state->{traversed_schema_path}.$state->{schema_path});
 
-  my $valid = 1;
+  $valid = E($state, 'metaschemas must have an $id')
+    if not length $state->{initial_schema_uri};
+
   my @vocabulary_classes;
   foreach my $uri (sort keys $schema->{'$vocabulary'}->%*) {
     $valid = 0, next if not assert_keyword_type({ %$state, _schema_path_suffix => $uri }, $schema, 'boolean');
     $valid = 0, next if not assert_uri({ %$state, _schema_path_suffix => $uri }, undef, $uri);
   }
-
-  $valid = E($state, 'metaschemas must have an $id')
-    if not length $state->{initial_schema_uri};
 
   # we cannot return an error here for invalid or incomplete vocabulary lists, because
   # - the specification vocabulary schemas themselves don't list Core,
