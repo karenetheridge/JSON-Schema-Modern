a772a66ff63664672e494e88f5bb1e37cb4fbd8a
Karen Etheridge <ether@cpan.org>
2021-12-03 08:47:35 -0800

    increment $VERSION after 0.530 release

--- lib/JSON/Schema/Modern/Vocabulary/FormatAssertion.pm
+++ lib/JSON/Schema/Modern/Vocabulary/FormatAssertion.pm
@@ -4,7 +4,7 @@ package JSON::Schema::Modern::Vocabulary::FormatAssertion;
 # vim: set ts=8 sts=2 sw=2 tw=100 et :
 # ABSTRACT: Implementation of the JSON Schema Format-Assertion vocabulary
 
-our $VERSION = '0.530';
+our $VERSION = '0.531';
 
 use 5.020;
 use Moo;
