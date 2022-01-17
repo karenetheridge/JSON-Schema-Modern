2c8d78b0f02fcba922d4a241a5e3606dd8bb1f89
Karen Etheridge <ether@cpan.org>
2022-01-17 11:00:52 -0800

    increment $VERSION after 0.540 release

--- lib/JSON/Schema/Modern/Vocabulary/Unevaluated.pm
+++ lib/JSON/Schema/Modern/Vocabulary/Unevaluated.pm
@@ -4,7 +4,7 @@ package JSON::Schema::Modern::Vocabulary::Unevaluated;
 # vim: set ts=8 sts=2 sw=2 tw=100 et :
 # ABSTRACT: Implementation of the JSON Schema Unevaluated vocabulary
 
-our $VERSION = '0.540';
+our $VERSION = '0.541';
 
 use 5.020;
 use Moo;
