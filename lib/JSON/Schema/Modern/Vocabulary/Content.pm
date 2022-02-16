493cf6989dd75c1cdce28c1328013b17777174cf
Karen Etheridge <ether@cpan.org>
2022-02-15 21:55:40 -0800

    increment $VERSION after 0.544 release

--- lib/JSON/Schema/Modern/Vocabulary/Content.pm
+++ lib/JSON/Schema/Modern/Vocabulary/Content.pm
@@ -4,7 +4,7 @@ package JSON::Schema::Modern::Vocabulary::Content;
 # vim: set ts=8 sts=2 sw=2 tw=100 et :
 # ABSTRACT: Implementation of the JSON Schema Content vocabulary
 
-our $VERSION = '0.544';
+our $VERSION = '0.545';
 
 use 5.020;
 use Moo;
