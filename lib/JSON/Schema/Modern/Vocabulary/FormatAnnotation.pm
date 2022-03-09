ce9f2ce304b03a3bbd8f416e55727f759a621c29
Karen Etheridge <ether@cpan.org>
2022-03-08 22:29:41 -0800

    increment $VERSION after 0.548 release

--- lib/JSON/Schema/Modern/Vocabulary/FormatAnnotation.pm
+++ lib/JSON/Schema/Modern/Vocabulary/FormatAnnotation.pm
@@ -4,7 +4,7 @@ package JSON::Schema::Modern::Vocabulary::FormatAnnotation;
 # vim: set ts=8 sts=2 sw=2 tw=100 et :
 # ABSTRACT: Implementation of the JSON Schema Format-Annotation vocabulary
 
-our $VERSION = '0.548';
+our $VERSION = '0.549';
 
 use 5.020;
 use Moo;
