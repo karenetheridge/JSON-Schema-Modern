ef9dc94d845bfdf52fb050a4ee2bd7e665ef1c42
Karen Etheridge <ether@cpan.org>
2022-04-13 21:34:55 -0700

    increment $VERSION after 0.550 release

--- lib/JSON/Schema/Modern/Vocabulary/Applicator.pm
+++ lib/JSON/Schema/Modern/Vocabulary/Applicator.pm
@@ -4,7 +4,7 @@ package JSON::Schema::Modern::Vocabulary::Applicator;
 # vim: set ts=8 sts=2 sw=2 tw=100 et :
 # ABSTRACT: Implementation of the JSON Schema Applicator vocabulary
 
-our $VERSION = '0.550';
+our $VERSION = '0.551';
 
 use 5.020;
 use Moo;
