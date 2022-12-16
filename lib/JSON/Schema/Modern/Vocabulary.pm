karenetheridge/JSON-Schema-Modern

commit 5cd1dee9208ec14ccca0b97a604243787c382661

perform vocabulary switcheroo only when needed

lib/JSON/Schema/Modern/Vocabulary/Core.pm
@@ -93,6 +93,15 @@ sub _eval_keyword_id ($self, $data, $schema, $state) {
   $state->{schema_path} = '';
   $state->{spec_version} = $schema_info->{specification_version};
   $state->{vocabularies} = $schema_info->{vocabularies};
+  # it's possible that a different schema resource has its own set of configs, different from the
+  # JSM attribute value
+  if ($state->{validate_formats}) {
+    $state->{vocabularies} = [
+      map s/^JSON::Schema::Modern::Vocabulary::Format\KAnnotation$/Assertion/r, $state->{vocabularies}->@*
+    ];
+    require JSON::Schema::Modern::Vocabulary::FormatAssertion;
+  }
+
   $state->@{keys $state->{configs}->%*} = values $state->{configs}->%*;
   push $state->{dynamic_scope}->@*, $state->{initial_schema_uri};
 

lib/JSON/Schema/Modern.pm
@@ -355,6 +355,13 @@ sub evaluate ($self, $data, $schema_reference, $config_override = {}) {
       } qw(validate_formats validate_content_schemas short_circuit collect_annotations scalarref_booleans strict)),
     };
 
+    if ($state->{validate_formats}) {
+      $state->{vocabularies} = [
+        map s/^JSON::Schema::Modern::Vocabulary::Format\KAnnotation$/Assertion/r, $state->{vocabularies}->@*
+      ];
+      require JSON::Schema::Modern::Vocabulary::FormatAssertion;
+    }
+
     $valid = $self->_eval_subschema($data, $schema_info->{schema}, $state);
     warn 'result is false but there are no errors' if not $valid and not $state->{errors}->@*;
   }
@@ -543,14 +550,8 @@ sub _eval_subschema ($self, $data, $schema, $state) {
   $state->{annotations} = [];
   my @new_annotations;
 
-  my @vocabularies = $state->{vocabularies}->@*; # override locally only (copy, not reference)
-  if ($state->{validate_formats}) {
-    s/^JSON::Schema::Modern::Vocabulary::Format\KAnnotation$/Assertion/ foreach @vocabularies;
-    require JSON::Schema::Modern::Vocabulary::FormatAssertion;
-  }
-
   ALL_KEYWORDS:
-  foreach my $vocabulary (@vocabularies) {
+  foreach my $vocabulary ($state->{vocabularies}->@*) {
     # [ [ $keyword => $subref|undef ], [ ... ] ]
     my $keyword_list = $vocabulary_cache->{$state->{spec_version}}{$vocabulary}{evaluate} //= [
       map [ $_ => $vocabulary->can('_eval_keyword_'.($_ =~ s/^\$//r)) ],

commit 0a6a83cbee524b6b9efd6899ba81edaa18a9335c

fix regression in format validation
introduced in commit 5cd1dee9

lib/JSON/Schema/Modern/Vocabulary.pm
@@ -72,6 +72,14 @@ sub eval_subschema_at_uri ($self, $data, $schema, $state, $uri) {
   my $schema_info = $state->{evaluator}->_fetch_from_uri($uri);
   abort($state, 'EXCEPTION: unable to find resource %s', $uri) if not $schema_info;
 
+  my $vocabularies = $schema_info->{vocabularies};
+  if ($state->{validate_formats}) {
+    $vocabularies = [
+      map s/^JSON::Schema::Modern::Vocabulary::Format\KAnnotation$/Assertion/r, $state->{vocabularies}->@*
+    ];
+    require JSON::Schema::Modern::Vocabulary::FormatAssertion;
+  }
+
   return $state->{evaluator}->_eval_subschema($data, $schema_info->{schema},
     +{
       $schema_info->{configs}->%*,
@@ -85,7 +93,7 @@ sub eval_subschema_at_uri ($self, $data, $schema, $state, $uri) {
       document_path => $schema_info->{document_path},
       spec_version => $schema_info->{specification_version},
       schema_path => '',
-      vocabularies => $schema_info->{vocabularies}, # reference, not copy
+      vocabularies => $vocabularies,
     });
 }
 

lib/JSON/Schema/Modern/Vocabulary/Core.pm
@@ -93,8 +93,7 @@ sub _eval_keyword_id ($self, $data, $schema, $state) {
   $state->{schema_path} = '';
   $state->{spec_version} = $schema_info->{specification_version};
   $state->{vocabularies} = $schema_info->{vocabularies};
-  # it's possible that a different schema resource has its own set of configs, different from the
-  # JSM attribute value
+
   if ($state->{validate_formats}) {
     $state->{vocabularies} = [
       map s/^JSON::Schema::Modern::Vocabulary::Format\KAnnotation$/Assertion/r, $state->{vocabularies}->@*