bb48f300daf0402171476e0e8f839bb0430724c1
Karen Etheridge <ether@cpan.org>
2022-12-21 16:03:26 -0800

    do not include space before colon when pretty-serializing json

--- lib/JSON/Schema/Modern/Annotation.pm
+++ lib/JSON/Schema/Modern/Annotation.pm
@@ -73,7 +73,7 @@ sub TO_JSON ($self) {
 }
 
 sub dump ($self) {
-  my $encoder = JSON::MaybeXS->new(utf8 => 0, convert_blessed => 1, canonical => 1, pretty => 1);
+  my $encoder = JSON::MaybeXS->new(utf8 => 0, convert_blessed => 1, canonical => 1, indent => 1, space_after => 1);
   $encoder->indent_length(2) if $encoder->can('indent_length');
   $encoder->encode($self);
 }
