6effba7856e162671edd1c70ff5de23c80b732f6
Karen Etheridge <ether@cpan.org>
2023-06-03 15:13:08 -0700

    increment $VERSION after 0.567 release

--- lib/JSON/Schema/Modern.pm
+++ lib/JSON/Schema/Modern.pm
@@ -5,7 +5,7 @@ package JSON::Schema::Modern;
 # ABSTRACT: Validate data against a schema
 # KEYWORDS: JSON Schema validator data validation structure specification
 
-our $VERSION = '0.567';
+our $VERSION = '0.568';
 
 use 5.020;  # for fc, unicode_strings features
 use Moo;
