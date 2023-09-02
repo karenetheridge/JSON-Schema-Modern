8c380a4bcf3393c726c7d799fc92039c69e0a133
Karen Etheridge <ether@cpan.org>
2023-09-02 13:43:59 -0700

    increment $VERSION after 0.570 release

--- lib/JSON/Schema/Modern.pm
+++ lib/JSON/Schema/Modern.pm
@@ -5,7 +5,7 @@ package JSON::Schema::Modern;
 # ABSTRACT: Validate data against a schema
 # KEYWORDS: JSON Schema validator data validation structure specification
 
-our $VERSION = '0.570';
+our $VERSION = '0.571';
 
 use 5.020;  # for fc, unicode_strings features
 use Moo;
--- lib/JSON/Schema/Modern/Vocabulary/Applicator.pm
+++ lib/JSON/Schema/Modern/Vocabulary/Applicator.pm
@@ -4,7 +4,7 @@ package JSON::Schema::Modern::Vocabulary::Applicator;
 # vim: set ts=8 sts=2 sw=2 tw=100 et :
 # ABSTRACT: Implementation of the JSON Schema Applicator vocabulary
 
-our $VERSION = '0.570';
+our $VERSION = '0.571';
 
 use 5.020;
 use Moo;
