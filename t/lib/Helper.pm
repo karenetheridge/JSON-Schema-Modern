4a260aefbfbcbeb71d02209411543812a168cc90
Karen Etheridge <ether@cpan.org>
2024-05-17 20:19:18 -0700

    add pragma goodies to the test helper

--- t/lib/Helper.pm
+++ t/lib/Helper.pm
@@ -1,5 +1,13 @@
-use strictures 2;
 # no package, so things defined here appear in the namespace of the parent.
+use strictures 2;
+use 5.020;
+use stable 0.031 'postderef';
+use experimental 'signatures';
+use if "$]" >= 5.022, experimental => 're_strict';
+no if "$]" >= 5.031009, feature => 'indirect';
+no if "$]" >= 5.033001, feature => 'multidimensional';
+no if "$]" >= 5.033006, feature => 'bareword_filehandles';
+use open ':std', ':encoding(UTF-8)'; # force stdin, stdout, stderr into utf8
 
 use Test::More 0.96;
 use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
