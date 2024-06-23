71fb8057e28a0f61097bf868bf79f49da53a5473
Karen Etheridge <ether@cpan.org>
2024-06-23 11:47:01 -0700

    increment $VERSION after 0.586 release

--- lib/JSON/Schema/Modern/Error.pm
+++ lib/JSON/Schema/Modern/Error.pm
@@ -4,7 +4,7 @@ package JSON::Schema::Modern::Error;
 # vim: set ts=8 sts=2 sw=2 tw=100 et :
 # ABSTRACT: Contains a single error from a JSON Schema evaluation
 
-our $VERSION = '0.586';
+our $VERSION = '0.587';
 
 use 5.020;
 use Moo;
