c0b1d6db7c16453e15b38c93cb33890231a7250a
Karen Etheridge <ether@cpan.org>
2025-02-28 15:38:02 -0800

    increment $VERSION after 0.603 release

--- lib/JSON/Schema/Modern/Vocabulary/MetaData.pm
+++ lib/JSON/Schema/Modern/Vocabulary/MetaData.pm
@@ -4,7 +4,7 @@ package JSON::Schema::Modern::Vocabulary::MetaData;
 # vim: set ts=8 sts=2 sw=2 tw=100 et :
 # ABSTRACT: Implementation of the JSON Schema Meta-Data vocabulary
 
-our $VERSION = '0.603';
+our $VERSION = '0.604';
 
 use 5.020;
 use Moo;
