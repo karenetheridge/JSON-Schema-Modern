bd65773b6eeb70cdb5c9f6491dcbd22c02a2c259
Karen Etheridge <ether@cpan.org>
2025-04-11 13:58:35 -0700

    increment $VERSION after 0.608 release

--- lib/JSON/Schema/Modern/Vocabulary/Core.pm
+++ lib/JSON/Schema/Modern/Vocabulary/Core.pm
@@ -4,7 +4,7 @@ package JSON::Schema::Modern::Vocabulary::Core;
 # vim: set ts=8 sts=2 sw=2 tw=100 et :
 # ABSTRACT: Implementation of the JSON Schema Core vocabulary
 
-our $VERSION = '0.608';
+our $VERSION = '0.609';
 
 use 5.020;
 use Moo;
