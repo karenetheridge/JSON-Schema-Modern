e7942f5f111e61baa1212188ae46fdd531150c02
Karen Etheridge <ether@cpan.org>
2025-07-12 09:14:17 -0700

    increment $VERSION after 0.615 release

--- lib/JSON/Schema/Modern/Annotation.pm
+++ lib/JSON/Schema/Modern/Annotation.pm
@@ -4,7 +4,7 @@ package JSON::Schema::Modern::Annotation;
 # vim: set ts=8 sts=2 sw=2 tw=100 et :
 # ABSTRACT: Contains a single annotation from a JSON Schema evaluation
 
-our $VERSION = '0.615';
+our $VERSION = '0.616';
 
 use 5.020;
 use Moo;
